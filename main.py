71513d96bfe1456d217356d330a1575f35074310
Yuya Tsuda <yytsuda@gmail.com>
2019-01-24 01:50:13 +0900

    Refactor

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from datetime import date
 import sqlite3
 from pathlib import Path
 
+# -- Constants
+# Location of the file: DayOne.sqlite
+DB_PATH = Path('~/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Documents/DayOne.sqlite').expanduser()
+# How many entries (from the latest) do you process?
+# 処理対象とするエントリの数
+COUNT = 200
+
+# ---
 class Entry:
     def __init__(self, pk, the_date, text):
         self.pk = pk  # primary key
@@ -9,14 +17,7 @@ class Entry:
         self.text = text
         # TODO: Add creation_datetime ??
 
-# Location of the file: DayOne.sqlite
-DB_PATH = Path('~/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Documents/DayOne.sqlite').expanduser()
-
-# ---
-# key: date
-# value: entry_count, char_count
-# daily_summary = dict()
-
+# -- Dictionaries for summary
 daily_entry_count = dict()
 daily_char_count = dict()
 
@@ -24,14 +25,13 @@ daily_char_count = dict()
 conn = sqlite3.connect(DB_PATH)
 c = conn.cursor()
 
-count = 100
+count = COUNT
 itr = c.execute('SELECT Z_PK, ZGREGORIANYEAR, ZGREGORIANMONTH, ZGREGORIANDAY, ZMARKDOWNTEXT FROM ZENTRY ORDER BY ZCREATIONDATE DESC LIMIT ?', (count,))
 for row in itr:
     pk,year,month,day,text = row
     entry = Entry(pk, date(year,month,day), text)
     # print(entry.pk, entry.date.isoformat(), entry.text[:5])
 
-    # daily_summary[entry.date]
     d = entry.date
     daily_entry_count[d] = daily_entry_count.get(d, 0) + 1
     daily_char_count[d] = daily_char_count.get(d, 0) + len(entry.text)
@@ -39,7 +39,11 @@ for row in itr:
 conn.close()
 # -- End of SQLite
 
+# ---
 # Show statistics
-# TODO: sort keys from the latest
-for d in daily_entry_count.keys():
+
+# Sort dates from the latest;
+# ignore the oldest day (because it may be wrong)
+dates = sorted(list(daily_entry_count.keys()), reverse=True)[:-1]
+for d in dates:
     print(d.isoformat(), daily_entry_count[d], daily_char_count[d])
