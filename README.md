eba9b429f4f0c448b647fc7e2b46d18b31a999b4
Yuya Tsuda <yytsuda@gmail.com>
2019-01-27 16:30:33 +0900

    Use argparse (#2)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Show statistics of your [Day One](http://dayoneapp.com/) journal.
 
 The statistics include...
-- the number of entries in a day (temporaly disabled)
+- the number of entries in a day
 - the total number of characters in a day
 
 Please note that this project is still under development.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import date
 import sqlite3
 from pathlib import Path
@@ -5,9 +6,26 @@ from pathlib import Path
 # -- Constants
 # Location of the file: DayOne.sqlite
 DB_PATH = Path('~/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Documents/DayOne.sqlite').expanduser()
-# How many entries (from the latest) do you process?
-# 処理対象とするエントリの数
+# The number of entries (from the latest one) to be processed.
 COUNT = 200
+
+# -- command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--daily-char',
+    action='store_true',
+    help='show the total number of characters in a day',
+    )
+parser.add_argument(
+    '--daily-entry',
+    action='store_true',
+    help='show the number of entries in a day',
+    )
+args = parser.parse_args()
+
+# If no options were specified, ...
+if not args.daily_entry and not args.daily_char:
+    args.daily_char = True
 
 # ---
 class Entry:
@@ -43,8 +61,11 @@ conn.close()
 # Show statistics
 
 # Sort dates from the latest;
-# ignore the oldest day (because it may be wrong)
+# Ignore the oldest day (because it may be wrong)
 dates = sorted(list(daily_entry_count.keys()), reverse=True)[:-1]
 for d in dates:
-    print(d.isoformat(), daily_char_count[d])
-    # print(d.isoformat(), daily_entry_count[d], daily_char_count[d])
+    print(
+        d.isoformat(),
+        daily_entry_count[d] if args.daily_entry else '',
+        daily_char_count[d] if args.daily_char else '',
+        )
